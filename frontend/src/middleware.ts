67811622e3458b11c6ab07eb067a80d9b865623c
Abhinav Katare <therockstarstar695@gmail.com>
2026-06-22 09:46:27 +0530

    Update comment for static files in middleware config
    Clarified comment regarding static files in matcher configuration.

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -9,7 +9,7 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
+     * - _next/static (static files) for matchers
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
